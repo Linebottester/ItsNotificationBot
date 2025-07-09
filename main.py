0098caecdfa0fc053039b89e186b09609d531253
Linebottester <vezem.bot.test@gmail.com>
2025-07-09 17:48:00 +0900

    test用の状態

--- main.py
+++ main.py
@@ -5,25 +5,23 @@ from scraper import scrape_avl_from_calender
 from db_utils import save_facilities
 from db_utils import fetch_wished_facilities
 from line_bot_utils import app, line_bot_api, handler
-import os
 import logging
-import requests
-import sqlite3
 
 # ロガー設定
 logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s")
 logger = logging.getLogger(__name__)
 
 def main():
-    logging.basicConfig(level=logging.INFO)
     
     # 施設の名前とURL一覧を取得
     facility_url = "https://linebottester.github.io/kenpo_test_site/test_calendar.html" # テスト用
     # facility_url = "https://as.its-kenpo.or.jp/apply/empty_calendar?s=PT13TjJjVFBrbG1KbFZuYzAxVFp5Vkhkd0YyWWZWR2JuOTJiblpTWjFKSGQ5a0hkdzFXWg%3D%3D&join_date=&night_count=1"
     
-    # 施設名と施設IDを取得する　毎回見に行くのはナンセンスな気がする　月初めのみに限定すべきか
-    facilities = scrape_facility_names_ids(facility_url)
 
+    # 施設名と施設IDを取得する　毎回見に行くのはナンセンスな気がする　月初めのみに限定すべきか
+    #　if count0or1over == 0 or datetime.today().day == 1:　#　例えばこんな感じとか
+
+    facilities = scrape_facility_names_ids(facility_url)
     save_facilities(facilities) #取得してきた施設と施設IDをDBへ保存
 
     # 希望されている施設IDと名前をDBから取得
@@ -32,10 +30,10 @@ def main():
     for wished_facility in wished_facilities:
         scrape_avl_from_calender(
             facility_id=wished_facility["facility_id"],
-            facility_name=wished_facility["facility_name"],  # 通知、ロガーなどに使うかも
+            facility_name=wished_facility["facility_name"],  # 通知、ロガーなどに使うので引数として渡しておく
             user_id=wished_facility["user_id"],              
             wish_date=wished_facility["wish_date"]           
         )
-
+    # count0or1over = 1 # 初回か否かの判定に用いる
 if __name__ == "__main__":
     main()
